76f412840a7a1d98473b8d82cd014ecb18c4f46c
AjKiin <kinichpakal603@gmail.com>
2024-06-05 09:01:50 -0700

    Adding subfolders for Glyphs
    Needed to organize the glyphs images a bit more

--- src/Data/Glyphs.js
+++ src/Data/Glyphs.js
@@ -5,155 +5,157 @@
 // ----------------- FILE PATHS ---------------------
 
 // Declare a set of imported image objects for numbers, 21x
-import glyph0 from '../Images/Glyphs/N0.png';
-import glyph1 from '../Images/Glyphs/N1.png';
-import glyph2 from '../Images/Glyphs/N2.png';
-import glyph3 from '../Images/Glyphs/N3.png';
-import glyph4 from '../Images/Glyphs/N4.png';
-import glyph5 from '../Images/Glyphs/N5.png';
-import glyph6 from '../Images/Glyphs/N6.png';
-import glyph7 from '../Images/Glyphs/N7.png';
-import glyph8 from '../Images/Glyphs/N8.png';
-import glyph9 from '../Images/Glyphs/N9.png';
-import glyph10 from '../Images/Glyphs/N10.png';
-import glyph11 from '../Images/Glyphs/N11.png';
-import glyph12 from '../Images/Glyphs/N12.png';
-import glyph13 from '../Images/Glyphs/N13.png';
-import glyph14 from '../Images/Glyphs/N14.png';
-import glyph15 from '../Images/Glyphs/N15.png';
-import glyph16 from '../Images/Glyphs/N16.png';
-import glyph17 from '../Images/Glyphs/N17.png';
-import glyph18 from '../Images/Glyphs/N18.png';
-import glyph19 from '../Images/Glyphs/N19.png';
-import glyphNull from '../Images/Glyphs/Null.png';
+import glyph0 from '../Images/Glyphs/Numbers/N0.png';
+import glyph1 from '../Images/Glyphs/Numbers/N1.png';
+import glyph2 from '../Images/Glyphs/Numbers/N2.png';
+import glyph3 from '../Images/Glyphs/Numbers/N3.png';
+import glyph4 from '../Images/Glyphs/Numbers/N4.png';
+import glyph5 from '../Images/Glyphs/Numbers/N5.png';
+import glyph6 from '../Images/Glyphs/Numbers/N6.png';
+import glyph7 from '../Images/Glyphs/Numbers/N7.png';
+import glyph8 from '../Images/Glyphs/Numbers/N8.png';
+import glyph9 from '../Images/Glyphs/Numbers/N9.png';
+import glyph10 from '../Images/Glyphs/Numbers/N10.png';
+import glyph11 from '../Images/Glyphs/Numbers/N11.png';
+import glyph12 from '../Images/Glyphs/Numbers/N12.png';
+import glyph13 from '../Images/Glyphs/Numbers/N13.png';
+import glyph14 from '../Images/Glyphs/Numbers/N14.png';
+import glyph15 from '../Images/Glyphs/Numbers/N15.png';
+import glyph16 from '../Images/Glyphs/Numbers/N16.png';
+import glyph17 from '../Images/Glyphs/Numbers/N17.png';
+import glyph18 from '../Images/Glyphs/Numbers/N18.png';
+import glyph19 from '../Images/Glyphs/Numbers/N19.png';
+import glyphNull from '../Images/Glyphs/Numbers/Null.png';
 
 
 // Declare a set of imported image objects for Tzolkin glyphs, 20x
-import glyphImix     from '../Images/Glyphs/Imix.png';
-import glyphIk       from '../Images/Glyphs/Ik.png';
-import glyphAkbal    from '../Images/Glyphs/Akbal.png';
-import glyphKan      from '../Images/Glyphs/Kan.png';
-import glyphChicchan from '../Images/Glyphs/Chicchan.png';
-import glyphCimi     from '../Images/Glyphs/Cimi.png';
-import glyphManik    from '../Images/Glyphs/Manik.png';
-import glyphLamat    from '../Images/Glyphs/Lamat.png';
-import glyphMuluc    from '../Images/Glyphs/Muluc.png';
-import glyphOc       from '../Images/Glyphs/Oc.png';
-import glyphChuen    from '../Images/Glyphs/Chuen.png';
-import glyphEb       from '../Images/Glyphs/Eb.png';
-import glyphBen      from '../Images/Glyphs/Ben.png';
-import glyphIx       from '../Images/Glyphs/Ix.png';
-import glyphMen      from '../Images/Glyphs/Men.png';
-import glyphCib      from '../Images/Glyphs/Cib.png';
-import glyphCaban    from '../Images/Glyphs/Caban.png';
-import glyphEtznab   from '../Images/Glyphs/Etznab.png';
-import glyphCauac    from '../Images/Glyphs/Cauac.png';
-import glyphAhau     from '../Images/Glyphs/Ahau.png';
+import glyphImix     from '../Images/Glyphs/Tzolkin/Imix.png';
+import glyphIk       from '../Images/Glyphs/Tzolkin/Ik.png';
+import glyphAkbal    from '../Images/Glyphs/Tzolkin/Akbal.png';
+import glyphKan      from '../Images/Glyphs/Tzolkin/Kan.png';
+import glyphChicchan from '../Images/Glyphs/Tzolkin/Chicchan.png';
+import glyphCimi     from '../Images/Glyphs/Tzolkin/Cimi.png';
+import glyphManik    from '../Images/Glyphs/Tzolkin/Manik.png';
+import glyphLamat    from '../Images/Glyphs/Tzolkin/Lamat.png';
+import glyphMuluc    from '../Images/Glyphs/Tzolkin/Muluc.png';
+import glyphOc       from '../Images/Glyphs/Tzolkin/Oc.png';
+import glyphChuen    from '../Images/Glyphs/Tzolkin/Chuen.png';
+import glyphEb       from '../Images/Glyphs/Tzolkin/Eb.png';
+import glyphBen      from '../Images/Glyphs/Tzolkin/Ben.png';
+import glyphIx       from '../Images/Glyphs/Tzolkin/Ix.png';
+import glyphMen      from '../Images/Glyphs/Tzolkin/Men.png';
+import glyphCib      from '../Images/Glyphs/Tzolkin/Cib.png';
+import glyphCaban    from '../Images/Glyphs/Tzolkin/Caban.png';
+import glyphEtznab   from '../Images/Glyphs/Tzolkin/Etznab.png';
+import glyphCauac    from '../Images/Glyphs/Tzolkin/Cauac.png';
+import glyphAhau     from '../Images/Glyphs/Tzolkin/Ahau.png';
 
 
 // Declare a set of imported image objects for Haab glyphs, 19x
-import glyphPop     from '../Images/Glyphs/Pop.png';
-import glyphUo      from '../Images/Glyphs/Uo.png';
-import glyphZip     from '../Images/Glyphs/Zip.png';
-import glyphZotz    from '../Images/Glyphs/Zotz.png';
-import glyphTzec    from '../Images/Glyphs/Tzec.png';
-import glyphXul     from '../Images/Glyphs/Xul.png';
-import glyphYaxkin  from '../Images/Glyphs/Yaxkin.png';
-import glyphMol     from '../Images/Glyphs/Mol.png';
-import glyphChen    from '../Images/Glyphs/Chen.png';
-import glyphYax     from '../Images/Glyphs/Yax.png';
-import glyphZac     from '../Images/Glyphs/Zac.png';
-import glyphCeh     from '../Images/Glyphs/Ceh.png';
-import glyphMac     from '../Images/Glyphs/Mac.png';
-import glyphKankin  from '../Images/Glyphs/Kankin.png';
-import glyphMuan    from '../Images/Glyphs/Muan.png';
-import glyphPax     from '../Images/Glyphs/Pax.png';
-import glyphKayab   from '../Images/Glyphs/Kayab.png';
-import glyphCumku   from '../Images/Glyphs/Cumku.png';
-import glyphUayeb   from '../Images/Glyphs/Uayeb.png';
+import glyphPop     from '../Images/Glyphs/Haab/Pop.png';
+import glyphUo      from '../Images/Glyphs/Haab/Uo.png';
+import glyphZip     from '../Images/Glyphs/Haab/Zip.png';
+import glyphZotz    from '../Images/Glyphs/Haab/Zotz.png';
+import glyphTzec    from '../Images/Glyphs/Haab/Tzec.png';
+import glyphXul     from '../Images/Glyphs/Haab/Xul.png';
+import glyphYaxkin  from '../Images/Glyphs/Haab/Yaxkin.png';
+import glyphMol     from '../Images/Glyphs/Haab/Mol.png';
+import glyphChen    from '../Images/Glyphs/Haab/Chen.png';
+import glyphYax     from '../Images/Glyphs/Haab/Yax.png';
+import glyphZac     from '../Images/Glyphs/Haab/Zac.png';
+import glyphCeh     from '../Images/Glyphs/Haab/Ceh.png';
+import glyphMac     from '../Images/Glyphs/Haab/Mac.png';
+import glyphKankin  from '../Images/Glyphs/Haab/Kankin.png';
+import glyphMuan    from '../Images/Glyphs/Haab/Muan.png';
+import glyphPax     from '../Images/Glyphs/Haab/Pax.png';
+import glyphKayab   from '../Images/Glyphs/Haab/Kayab.png';
+import glyphCumku   from '../Images/Glyphs/Haab/Cumku.png';
+import glyphUayeb   from '../Images/Glyphs/Haab/Uayeb.png';
 
 // Declare a set of imported image objects for Lord of the Night glyphs, 9x
 // Taken from Mark Pitts "Maya Numbers and Calendar Book 2"
-import glyphLord1   from '../Images/Glyphs/Lord1.png';
-import glyphLord2   from '../Images/Glyphs/Lord2.png';
-import glyphLord3   from '../Images/Glyphs/Lord3.png';
-import glyphLord4   from '../Images/Glyphs/Lord4.png';
-import glyphLord5   from '../Images/Glyphs/Lord5.png';
-import glyphLord6   from '../Images/Glyphs/Lord6.png';
-import glyphLord7   from '../Images/Glyphs/Lord7.png';
-import glyphLord8   from '../Images/Glyphs/Lord8.png';
-import glyphLord9   from '../Images/Glyphs/Lord9.png';
+import glyphLord1   from '../Images/Glyphs/Lords/Lord1.png';
+import glyphLord2   from '../Images/Glyphs/Lords/Lord2.png';
+import glyphLord3   from '../Images/Glyphs/Lords/Lord3.png';
+import glyphLord4   from '../Images/Glyphs/Lords/Lord4.png';
+import glyphLord5   from '../Images/Glyphs/Lords/Lord5.png';
+import glyphLord6   from '../Images/Glyphs/Lords/Lord6.png';
+import glyphLord7   from '../Images/Glyphs/Lords/Lord7.png';
+import glyphLord8   from '../Images/Glyphs/Lords/Lord8.png';
+import glyphLord9   from '../Images/Glyphs/Lords/Lord9.png';
 
 // Declare a set of imported image objects for the Long Count, 6x
 // Taken from Mark Pitts "Maya Numbers and Calendar Book 2"
-import glyphKin      from '../Images/Glyphs/Kin.png';
-import glyphWinal    from '../Images/Glyphs/Winal.png';
-import glyphTun      from '../Images/Glyphs/Tun.png';
-import glyphKatun    from '../Images/Glyphs/Katun.png';
-import glyphBaktun   from '../Images/Glyphs/Baktun.png';
-import glyphPiktun   from '../Images/Glyphs/Piktun.png';
-import glyphKalabtun from '../Images/Glyphs/Kalabtun.png';
+import glyphKin      from '../Images/Glyphs/LongCount/Kin.png';
+import glyphWinal    from '../Images/Glyphs/LongCount/Winal.png';
+import glyphTun      from '../Images/Glyphs/LongCount/Tun.png';
+import glyphKatun    from '../Images/Glyphs/LongCount/Katun.png';
+import glyphBaktun   from '../Images/Glyphs/LongCount/Baktun.png';
+import glyphPiktun   from '../Images/Glyphs/LongCount/Piktun.png';
+import glyphKalabtun from '../Images/Glyphs/LongCount/Kalabtun.png';
 
 // Declare a set of imported image objects for the ISIG, 18x
-import glyphISIGPop     from '../Images/Glyphs/ISIGPop.png';
-import glyphISIGUo      from '../Images/Glyphs/ISIGUo.png';
-import glyphISIGZip     from '../Images/Glyphs/ISIGZip.png';
-import glyphISIGZotz    from '../Images/Glyphs/ISIGZotz.png';
-import glyphISIGTzec    from '../Images/Glyphs/ISIGTzec.png';
-import glyphISIGXul     from '../Images/Glyphs/ISIGXul.png';
-import glyphISIGYaxkin  from '../Images/Glyphs/ISIGYaxkin.png';
-import glyphISIGMol     from '../Images/Glyphs/ISIGMol.png';
-import glyphISIGChen    from '../Images/Glyphs/ISIGChen.png';
-import glyphISIGYax     from '../Images/Glyphs/ISIGYax.png';
-import glyphISIGZac     from '../Images/Glyphs/ISIGZac.png';
-import glyphISIGCeh     from '../Images/Glyphs/ISIGCeh.png';
-import glyphISIGMac     from '../Images/Glyphs/ISIGMac.png';
-import glyphISIGKankin  from '../Images/Glyphs/ISIGKankin.png';
-import glyphISIGMuan    from '../Images/Glyphs/ISIGMuan.png';
-import glyphISIGPax     from '../Images/Glyphs/ISIGPax.png';
-import glyphISIGKayab   from '../Images/Glyphs/ISIGKayab.png';
-import glyphISIGCumku   from '../Images/Glyphs/ISIGCumku.png';
-import glyphISIGUayeb   from '../Images/Glyphs/ISIGUayeb.png';
+import glyphISIGPop     from '../Images/Glyphs/ISIG/ISIGPop.png';
+import glyphISIGUo      from '../Images/Glyphs/ISIG/ISIGUo.png';
+import glyphISIGZip     from '../Images/Glyphs/ISIG/ISIGZip.png';
+import glyphISIGZotz    from '../Images/Glyphs/ISIG/ISIGZotz.png';
+import glyphISIGTzec    from '../Images/Glyphs/ISIG/ISIGTzec.png';
+import glyphISIGXul     from '../Images/Glyphs/ISIG/ISIGXul.png';
+import glyphISIGYaxkin  from '../Images/Glyphs/ISIG/ISIGYaxkin.png';
+import glyphISIGMol     from '../Images/Glyphs/ISIG/ISIGMol.png';
+import glyphISIGChen    from '../Images/Glyphs/ISIG/ISIGChen.png';
+import glyphISIGYax     from '../Images/Glyphs/ISIG/ISIGYax.png';
+import glyphISIGZac     from '../Images/Glyphs/ISIG/ISIGZac.png';
+import glyphISIGCeh     from '../Images/Glyphs/ISIG/ISIGCeh.png';
+import glyphISIGMac     from '../Images/Glyphs/ISIG/ISIGMac.png';
+import glyphISIGKankin  from '../Images/Glyphs/ISIG/ISIGKankin.png';
+import glyphISIGMuan    from '../Images/Glyphs/ISIG/ISIGMuan.png';
+import glyphISIGPax     from '../Images/Glyphs/ISIG/ISIGPax.png';
+import glyphISIGKayab   from '../Images/Glyphs/ISIG/ISIGKayab.png';
+import glyphISIGCumku   from '../Images/Glyphs/ISIG/ISIGCumku.png';
+import glyphISIGUayeb   from '../Images/Glyphs/ISIG/ISIGUayeb.png';
 
 // Declare a set of imported image objects for lettered glyphs, 5x
 // Taken from Mark Pitts "Maya Numbers and Calendar Book 2"
-import glyphF  from '../Images/Glyphs/F.png';
-import glyphD  from '../Images/Glyphs/D.png';
-import glyphE  from '../Images/Glyphs/E.png';
-import glyphC  from '../Images/Glyphs/C.png';
-import glyphY  from '../Images/Glyphs/Y.png';
+import glyphF  from '../Images/Glyphs/Various/F.png';
+import glyphD  from '../Images/Glyphs/Various/D.png';
+import glyphE  from '../Images/Glyphs/Various/E.png';
+import glyphC  from '../Images/Glyphs/Various/C.png';
+import glyphY  from '../Images/Glyphs/Various/Y.png';
 
 // These are from J. Montgomery "How to Read Maya Hieroglyphs"
-import glyphB    from '../Images/Glyphs/B.png'; 
-import glyphA29  from '../Images/Glyphs/A29.png';
-import glyphA30  from '../Images/Glyphs/A30.png';
-import glyphX1   from '../Images/Glyphs/X1.png';
-import glyphX2   from '../Images/Glyphs/X2.png';
-import glyphX3   from '../Images/Glyphs/X3.png';
-import glyphX4   from '../Images/Glyphs/X4.png';
-import glyphX5   from '../Images/Glyphs/X5.png';
-import glyphX6   from '../Images/Glyphs/X6.png';
+import glyphB    from '../Images/Glyphs/Various/B.png'; 
+import glyphA29  from '../Images/Glyphs/Various/A29.png';
+import glyphA30  from '../Images/Glyphs/Various/A30.png';
+import glyphX1   from '../Images/Glyphs/Various/X1.png';
+import glyphX2   from '../Images/Glyphs/Various/X2.png';
+import glyphX3   from '../Images/Glyphs/Various/X3.png';
+import glyphX4   from '../Images/Glyphs/Various/X4.png';
+import glyphX5   from '../Images/Glyphs/Various/X5.png';
+import glyphX6   from '../Images/Glyphs/Various/X6.png';
 
 // Declare a set of imported image objects for directions and colors, 8x
 // Taken from Mark Pitts "Writing in Maya Glyphs Book 1"
-import glyphEast    from '../Images/Glyphs/East.png';
-import glyphNorth   from '../Images/Glyphs/North.png';
-import glyphWest    from '../Images/Glyphs/West.png';
-import glyphSouth   from '../Images/Glyphs/South.png';
-import glyphRed     from '../Images/Glyphs/Red.png';
-import glyphBlack   from '../Images/Glyphs/Black.png';
-import glyphYellow  from '../Images/Glyphs/Yellow.png';
-import glyphWhite   from '../Images/Glyphs/White.png';
+import glyphEast    from '../Images/Glyphs/Directions/East.png';
+import glyphNorth   from '../Images/Glyphs/Directions/North.png';
+import glyphWest    from '../Images/Glyphs/Directions/West.png';
+import glyphSouth   from '../Images/Glyphs/Directions/South.png';
+import glyphRed     from '../Images/Glyphs/Colors/Red.png';
+import glyphBlack   from '../Images/Glyphs/Colors/Black.png';
+import glyphYellow  from '../Images/Glyphs/Colors/Yellow.png';
+import glyphWhite   from '../Images/Glyphs/Colors/White.png';
 
 // Declare a set of imported image objects for various assorted glyhs
-import glyph819verb  from '../Images/Glyphs/819verb.png';
-import glyphKawil    from '../Images/Glyphs/Kawil.png';
-import glyphDNIG     from '../Images/Glyphs/DNIG.png';
-import glyphADI      from '../Images/Glyphs/ADI.png';
-import glyphUnknown  from '../Images/Glyphs/Unknown.png';
+import glyph819verb  from '../Images/Glyphs/Various/819verb.png';
+import glyphKawil    from '../Images/Glyphs/Various/Kawil.png';
+import glyphDNIG     from '../Images/Glyphs/Various/DNIG.png';
+import glyphADI      from '../Images/Glyphs/Various/ADI.png';
+import glyphUnknown  from '../Images/Glyphs/Various/Unknown.png';
 
 
 // ----------------- DATA ARRAYS ---------------------
+
+// NOTE: Order matters for all of the arrays below!
 
 // Create an array to store the image object names for numbers
 const numberGlyphPaths = [
@@ -209,7 +211,7 @@ const colorGlyphPaths = [ glyphWhite, glyphBlack, glyphYellow, glyphRed, ];
 // The order of the tooltips is this...
 // ISIG, Baktun, Katun, Tun, Uinal, Kin, Tzolkin, Lord of Night, F, 
 // D, E, C, X, B, A, YZ, Haab, 
-// DNIG, ADI, Tun, Uinal, Kin, Tzolkin, Haab, 819 verb, Direction, Color, Kawil, Unknown
+// DNIG, ADI, Tun, Uinal, Kin, Tzolkin, Haab, 819 verb, Direction, Color, Kawil, Unknown, Piktun, Kalabtun
 const toolTipMessages = [
     "ISIG - An oversized Initial Series Introductory Glyph with Haab month patron deities infixed",
 
